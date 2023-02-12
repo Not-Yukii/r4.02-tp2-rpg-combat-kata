b13fb49499d452b5d55a00648397e465bf739350
Ludo <ludovic.pradel@gmail.com>
2023-02-12 23:13:20 +0100

    adding maven project structure

---
+++ src/main/java/iut/Player.java
@@ -0,0 +1,6 @@
+package iut;
+
+class Player {
+
+}
+
---
+++ src/test/java/iut/PlayerTest.java
@@ -0,0 +1,15 @@
+package iut;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+
+    @Test
+    public void test() {
+        assertThat(1 + 2).isEqualTo(3);
+    }
+
+}
+
