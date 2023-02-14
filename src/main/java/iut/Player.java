3f9c2e99d2c3d1529aa4e87b4fb5ce6aa0f975af
Yukii <96383296+Not-Yukii@users.noreply.github.com>
2023-02-14 16:36:01 +0100

    étape 2

--- src/main/java/iut/Player.java
+++ src/main/java/iut/Player.java
@@ -2,14 +2,39 @@ package iut;
 
 public class Player {
     private int healthPoints;
+    private boolean alive;
 
     public Player() {
         this.healthPoints = 100;
+        this.alive = true;
     }
 
-    public int health() {
+    public int getHealthPoints() {
         return this.healthPoints;
     }
-    
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+        if (this.healthPoints <= 0) {
+            this.healthPoints = 0;
+            this.alive = false;
+        }
+    }
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    public void hit(Player opponent) {
+        opponent.takeDamage(10);
+    }
+
+    public void takeDamage(int damage) {
+        this.healthPoints -= damage;
+        if (this.healthPoints <= 0) {
+            this.healthPoints = 0;
+            this.alive = false;
+        }
+    }
 }
 
--- src/test/java/iut/PlayerTest.java
+++ src/test/java/iut/PlayerTest.java
@@ -1,5 +1,9 @@
 package iut;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,20 +14,52 @@ public class PlayerTest {
     public void test() {
         assertThat(1 + 2).isEqualTo(3);
     }
-
-
+    
+    @Test
+    public void testIsAlive() {
+        Player player1 = new Player();
+        assertTrue(player1.isAlive());
+        player1.setHealthPoints(0);
+        assertFalse(player1.isAlive());
+    }
+    
+    @Test
+    public void testTakeDamage() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        player1.takeDamage(50);
+        assertEquals(50, player1.getHealthPoints());
+        player2.takeDamage(120);
+        assertEquals(0, player2.getHealthPoints());
+        assertFalse(player2.isAlive());
+    }
+    
+    @Test
+    public void testHit() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        player1.hit(player2);
+        assertEquals(90, player2.getHealthPoints());
+        assertTrue(player2.isAlive());
+        player2.hit(player1);
+        assertEquals(90, player1.getHealthPoints());
+        assertTrue(player1.isAlive());
+        player2.hit(player1);
+        assertEquals(80, player1.getHealthPoints());
+        assertTrue(player1.isAlive());
+    }
 
     @Test
-        public void ShouldCreatePlayer() {
-            // GIVEN
-            Player hunter = new Player();
+    public void ShouldCreatePlayer() {
+        // GIVEN
+        Player hunter = new Player();
 
-            // WHEN
-            int initialHealthPoints = hunter.health();
+        // WHEN
+        int initialHealthPoints = hunter.getHealthPoints();
 
-            // THEN
-            assertThat(initialHealthPoints).isEqualTo(100);
+        // THEN
+        assertThat(initialHealthPoints).isEqualTo(100);
 
-        }
+    }
 }
 
